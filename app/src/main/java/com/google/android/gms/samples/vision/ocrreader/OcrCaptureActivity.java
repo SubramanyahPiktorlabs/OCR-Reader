ed7ed2b4e15e6158b38d0c1f2833abcf8511d361
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-03-26 13:24:59 +0530

    Toast message for context testing

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
@@ -92,6 +92,11 @@ public class OcrCaptureActivity extends AppCompatActivity {
     }
 
     public void sayHello(Context reactContext, Activity getCurrentActivity){
+        if(reactContext != null){
+            Toast.makeText(reactContext,"Non Null Context",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(reactContext,"Null Context",Toast.LENGTH_LONG).show();
+        }
         Toast.makeText(getCurrentActivity.getApplicationContext(),"Undya BVC",Toast.LENGTH_LONG).show();
     }
 
